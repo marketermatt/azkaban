5053d8c8ebe85761710a4bf4de3da6aa6249a3d9
gaganphp <mailtogagan990@gmail.com>
2016-06-19 17:10:58 -0400

    Updated woocommerce templates
    Updated woocommerce templates

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -1,10 +1,14 @@
 <?php
 /**
- * Variable product add to cart
+ * Single variation display
  *
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     2.4.0
+ * This is a javascript-based template for single variations (see https://codex.wordpress.org/Javascript_Reference/wp.template).
+ * The values will be dynamically replaced after selecting attributes.
+ *
+ * @see 	https://docs.woothemes.com/document/template-structure/
+ * @author  WooThemes
+ * @package WooCommerce/Templates
+ * @version 2.5.0
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly
