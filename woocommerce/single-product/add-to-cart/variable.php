68fadf0de53755161c7e175f5b0b5382bacb9bd4
gaganphp <mailtogagan990@gmail.com>
2016-08-15 02:58:26 +0530

    updated woocommerce templates

--- woocommerce/single-product/add-to-cart/variable.php
+++ woocommerce/single-product/add-to-cart/variable.php
@@ -1,11 +1,16 @@
 <?php
 /**
- * Single variation display
+ * Variable product add to cart
  *
- * This is a javascript-based template for single variations (see https://codex.wordpress.org/Javascript_Reference/wp.template).
- * The values will be dynamically replaced after selecting attributes.
+ * This template can be overridden by copying it to yourtheme/woocommerce/single-product/add-to-cart/variable.php.
  *
- * @see 	https://docs.woothemes.com/document/template-structure/
+ * HOWEVER, on occasion WooCommerce will need to update template files and you
+ * (the theme developer) will need to copy the new files to your theme to
+ * maintain compatibility. We try to do this as little as possible, but it does
+ * happen. When this occurs the version of the template file will be bumped and
+ * the readme will list any important changes.
+ *
+ * @see 	https://docs.woocommerce.com/document/template-structure/
  * @author  WooThemes
  * @package WooCommerce/Templates
  * @version 2.5.0
